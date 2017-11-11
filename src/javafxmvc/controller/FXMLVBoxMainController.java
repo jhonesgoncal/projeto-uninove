373eaffebd50305d70effc30262f341db68ff5c2
Jhones Goncalves <jhones.goncalves@outlook.com>
2017-11-11 13:27:54 -0200

    subindo aplicacao

---
+++ src/javafxmvc/controller/FXMLVBoxMainController.java
@@ -0,0 +1,46 @@
+package javafxmvc.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+public class FXMLVBoxMainController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemCadastrosCategorias;
+    @FXML
+    private MenuItem menuItemCadastrosClientes;
+    @FXML
+    private MenuItem menuItemCadastrosProdutos;
+    @FXML
+    private MenuItem menuItemProcessosVendas;
+    @FXML
+    private MenuItem menuItemGraficosVendasPorMes;
+    @FXML
+    private MenuItem menuItemRelatoriosQuantidades;
+    @FXML
+    private AnchorPane anchorPane;
+    
+            
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+   
+    
+    public void handleMenuItemCadastroClientes() throws IOException{
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafxmvc/view/FXMLAnchorPaneCadastroClientes.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+    
+    
+    public void handleMenuItemProcessosVendas() throws IOException{
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafxmvc/view/FXMLAnchorPaneProcessosVendas.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+}
